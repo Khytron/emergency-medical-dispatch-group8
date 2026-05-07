a27c813c18341041db7f3a1546494120cc5a35bd
M. Ikhmal Hakimie <24001471@siswa.um.edu.my>
2026-05-07 10:26:55 +0800

    Initialized Emergency Medical Dispatch project (Java with Maven)

---
+++ src/main/java/com/mycompany/emergency_medical_dispatch/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.emergency_medical_dispatch;
+
+/**
+ *
+ * @author MSI GF63 THIN 8RCS
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello team!");
+    }
+}
